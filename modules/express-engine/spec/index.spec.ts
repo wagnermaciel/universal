a06d7daae98ced5c71215fe5d64f3fde44fcd814
Fabian Wiles <fabianwiles@live.com>
2018-04-07 08:18:54 +1200

    WIP test(express-engine): add test with express rendering

--- modules/express-engine/spec/index.spec.ts
+++ modules/express-engine/spec/index.spec.ts
@@ -1,5 +1,37 @@
+function makeExpressInstance(module: NgModuleFactory<any>) {
+  const app = express();
+  app.engine('html', ngExpressEngine({
+    bootstrap: module
+  }));
+
+  app.set('view engine', 'html');
+
+  app.get('*', (req: express.Request, res: express.Response) => {
+    // TODO: how to render without express trying to lookup a view?
+    (res as any).render(null, {
+      req,
+      res,
+      document: '<root></root>'
+    });
+  });
+  return app;
+}
+
 describe('test runner', () => {
   it('can run a test', () => {
     expect(true).toBe(true);
+  });
+  xit('can render a simple app', (done) => {
+    const template = `some template: ${new Date()}`;
+    const appModule = makeTestingModule(template);
+    const expressApp = makeExpressInstance(appModule);
+    expressApp.listen(3000, () => {
+      axios.get('http://localhost:3000')
+      .then(res => res.data)
+      .then(html => {
+        expect(html).toContain(template);
+        done();
+      });
+    });
   });
 });
